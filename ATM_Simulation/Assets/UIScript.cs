b41f44908332acbf92f61f5165d84dbc1943432a
Franz Cadiente <fcadien1@my.centennialcollege.ca>
2022-02-19 22:51:44 -0500

    Added UI and improved ATM

---
+++ ATM_Simulation/Assets/UIScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIScript : MonoBehaviour
+{
+    //use this bool field to set the text on top of ATM
+    public bool isFree;
+    public Text statusText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        statusText = this.GetComponentInChildren<Text>();
+        isFree = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFree)
+        {
+            statusText.text = "Free";
+            statusText.color = Color.black;
+        }
+        else
+        {
+            statusText.text = "In Use";
+            statusText.color = Color.red;
+        }
+    }
+}
